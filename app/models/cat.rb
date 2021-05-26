c955def1d3a17135595c2e31c4f0883f0a42d5db
Ramsey Njire <ramseynjire@gmail.com>
2021-05-26 22:38:09 +0300

    write model validations for cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,7 @@
 class Cat < ApplicationRecord
+    ALLOWED_COLORS = ["black", "white", "brown", "yellow", "orange"]
+    ALLOWED_SEXES = ["m", "f"]
+    validates :birthdate, :color, :name, :sex, presence: true
+    validates_inclusion_of :color, in: ALLOWED_COLORS
+    validates_inclusion_of :sex, in: ALLOWED_SEXES
 end
