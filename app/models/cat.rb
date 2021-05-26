d5ce15bfd5d85b901bf376471bea0478cf7d61a8
Ramsey Njire <ramseynjire@gmail.com>
2021-05-26 23:47:43 +0300

    write age method for cats

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,8 @@
 class Cat < ApplicationRecord
+    require "action_view"
+    include ActionView::Helpers::DateHelper
+
+
     ALLOWED_COLORS = ["black", "white", "brown", "yellow", "orange"]
     ALLOWED_SEXES = ["m", "f"]
     validates :birthdate, :color, :name, :sex, presence: true
@@ -6,4 +10,9 @@ class Cat < ApplicationRecord
     validates_length_of :sex, maximum: 1
     validates_inclusion_of :color, in: ALLOWED_COLORS
     validates_inclusion_of :sex, in: ALLOWED_SEXES
+
+
+    def age
+        time_ago_in_words(birthdate)
+    end
 end
