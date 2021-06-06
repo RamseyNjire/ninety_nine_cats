b23f691ca7d40356c0d531ca4364072a4279f679
Ramsey Njire <ramseynjire@gmail.com>
2021-06-07 00:06:19 +0300

    finish new and create controller actions and views for cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,23 +4,21 @@ class CatsController < ApplicationController
         render :index
     end
 
-    # def new
-    #     @cat = Cat.new
-    #     @allowed_colors = Cat::ALLOWED_COLORS
-    #     @allowed_sexes = Cat::ALLOWED_SEXES
-    #     render :new
-    # end
+    def new
+        @cat = Cat.new
+        @allowed_colors = Cat::ALLOWED_COLORS
+        render :new
+    end
 
-    # def create
-    #     @cat = Cat.new(cat_params)
-    #     @allowed_colors = Cat::ALLOWED_COLORS
-    #     @allowed_sexes = Cat::ALLOWED_SEXES
-    #     if @cat.save
-    #         redirect_to cat_url(@cat)
-    #     else
-    #         render :new
-    #     end
-    # end
+    def create
+        @cat = Cat.new(cat_params)
+        # @allowed_colors = Cat::ALLOWED_COLORS
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
 
     def show
         @cat = Cat.find_by(id: params[:id])
@@ -47,6 +45,6 @@ class CatsController < ApplicationController
 
     private
     def cat_params
-        params.require(:cat).permit(:name, :birthdate, :color, :sex)
+        params.require(:cat).permit(:name, :birthdate, :color, :sex, :description)
     end
 end
