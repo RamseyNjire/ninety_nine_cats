b4d7895451e2e138c97a4baf2d2f702c646e0cf5
Ramsey Njire <ramseynjire@gmail.com>
2021-06-06 23:33:35 +0300

    rebuild index and show pages

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,28 +4,46 @@ class CatsController < ApplicationController
         render :index
     end
 
-    def new
-        @cat = Cat.new
-        @allowed_colors = Cat::ALLOWED_COLORS
-        @allowed_sexes = Cat::ALLOWED_SEXES
-        render :new
-    end
+    # def new
+    #     @cat = Cat.new
+    #     @allowed_colors = Cat::ALLOWED_COLORS
+    #     @allowed_sexes = Cat::ALLOWED_SEXES
+    #     render :new
+    # end
 
-    def create
-        @cat = Cat.new(cat_params)
-        @allowed_colors = Cat::ALLOWED_COLORS
-        @allowed_sexes = Cat::ALLOWED_SEXES
-        if @cat.save
-            redirect_to cat_url(@cat)
-        else
-            render :new
-        end
-    end
+    # def create
+    #     @cat = Cat.new(cat_params)
+    #     @allowed_colors = Cat::ALLOWED_COLORS
+    #     @allowed_sexes = Cat::ALLOWED_SEXES
+    #     if @cat.save
+    #         redirect_to cat_url(@cat)
+    #     else
+    #         render :new
+    #     end
+    # end
 
     def show
-        @cat = Cat.find(params[:id])
+        @cat = Cat.find_by(id: params[:id])
         render :show
     end
+
+    # def edit
+    #     @cat = Cat.find_by(id: params[:id])
+    #     @allowed_colors = Cat::ALLOWED_COLORS
+    #     @allowed_sexes = Cat::ALLOWED_SEXES
+
+    #     render :edit
+    # end
+
+    # def update
+    #     @cat = Cat.find_by(id: params[:id])
+
+    #     if @cat.update_attributes(cat_params)
+    #         redirect_to cats_url
+    #     else
+    #         render :edit
+    #     end
+    # end
 
     private
     def cat_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ramsey = Cat.create(birthdate: "2015-09-08", color: "black", name: "Ramsey", sex: "m", description: "This cat is curious, has a quirky sense of humour, and loves chicken")
-reson = Cat.create(birthdate: "2019-09-23", color: "white", name: "Reson", sex: "f", description: "This cat is laid back, has a dark sense of humour, and loves to watch TV")
-gideon = Cat.create(birthdate: "2016-06-01", color: "brown", name: "Gideon", sex: "m", description: "This cat is easygoing, loves slapstick humour, and enjoys scrolling through Instagram")
+ramsey = Cat.create(birthdate: "2015-09-08", color: "black", name: "Ramsey", sex: "M", description: "This cat is curious, has a quirky sense of humour, and loves chicken")
+reson = Cat.create(birthdate: "2019-09-23", color: "white", name: "Reson", sex: "F", description: "This cat is laid back, has a dark sense of humour, and loves to watch TV")
+gideon = Cat.create(birthdate: "2016-06-01", color: "brown", name: "Gideon", sex: "M", description: "This cat is easygoing, loves slapstick humour, and enjoys scrolling through Instagram")
 
 
