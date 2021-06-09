f60dfeb7b60d78fd2dc8343d7458db956c8a23cd
Ramsey Njire <ramseynjire@gmail.com>
2021-06-09 20:25:58 +0300

    write new and create controller methods for cat rental requests, as well as view for rental requests

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -5,4 +5,20 @@ class CatRentalRequestsController < ApplicationController
         @status_values = CatRentalRequest::STATUS_VALUES
         render :new
     end
+
+    def create
+        request = CatRentalRequest.new(cat_rental_request_params)
+
+        if request.save
+            render plain: "New request created!"
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def cat_rental_request_params
+        params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date)
+    end
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,7 +2,7 @@
 #
 # Table name: cats
 #
-#  id          :integer          not null, primary key
+#  id          :bigint           not null, primary key
 #  birthdate   :date             not null
 #  color       :string           not null
 #  name        :string           not null
