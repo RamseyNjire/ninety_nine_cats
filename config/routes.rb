2fc681e3e5bdc6f95b4f7cf32fb1831ed5ccaa7d
Ramsey Njire <ramseynjire@gmail.com>
2021-06-15 15:08:49 +0300

    reset files to start project afresh

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,3 @@
-# == Route Map
-#
-#                     Prefix Verb   URI Pattern                                                                              Controller#Action
-#                       cats GET    /cats(.:format)                                                                          cats#index
-#                            POST   /cats(.:format)                                                                          cats#create
-#                    new_cat GET    /cats/new(.:format)                                                                      cats#new
-#                   edit_cat GET    /cats/:id/edit(.:format)                                                                 cats#edit
-#                        cat GET    /cats/:id(.:format)                                                                      cats#show
-#                            PATCH  /cats/:id(.:format)                                                                      cats#update
-#                            PUT    /cats/:id(.:format)                                                                      cats#update
-# approve_cat_rental_request POST   /cat_rental_requests/:id/approve(.:format)                                               cat_rental_requests#approve
-#    deny_cat_rental_request POST   /cat_rental_requests/:id/deny(.:format)                                                  cat_rental_requests#deny
-#        cat_rental_requests POST   /cat_rental_requests(.:format)                                                           cat_rental_requests#create
-#     new_cat_rental_request GET    /cat_rental_requests/new(.:format)                                                       cat_rental_requests#new
-#                      users GET    /users(.:format)                                                                         users#index
-#                            POST   /users(.:format)                                                                         users#create
-#                   new_user GET    /users/new(.:format)                                                                     users#new
-#                  edit_user GET    /users/:id/edit(.:format)                                                                users#edit
-#                       user GET    /users/:id(.:format)                                                                     users#show
-#                            PATCH  /users/:id(.:format)                                                                     users#update
-#                            PUT    /users/:id(.:format)                                                                     users#update
-#                            DELETE /users/:id(.:format)                                                                     users#destroy
-#                new_session GET    /session/new(.:format)                                                                   sessions#new
-#               edit_session GET    /session/edit(.:format)                                                                  sessions#edit
-#                    session GET    /session(.:format)                                                                       sessions#show
-#                            PATCH  /session(.:format)                                                                       sessions#update
-#                            PUT    /session(.:format)                                                                       sessions#update
-#                            DELETE /session(.:format)                                                                       sessions#destroy
-#                            POST   /session(.:format)                                                                       sessions#create
-#                       root GET    /                                                                                        redirect(301, /cats)
-#         rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
-#  rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
-#         rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
-#  update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
-#       rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
-
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats, except: :destroy
@@ -43,8 +7,6 @@ Rails.application.routes.draw do
       post :deny
     end
   end
-  resources :users
-  resource :session
 
   root to: redirect('/cats')
 end
