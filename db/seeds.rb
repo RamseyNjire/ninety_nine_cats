44943446fbb8f7597ae4f8f37194ec7c20b46f17
Ramsey Njire <ramseynjire@gmail.com>
2021-06-15 15:23:13 +0300

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ramsey = Cat.create(birthdate: "2015-09-08", color: "black", name: "Ramsey", sex: "M", description: "This cat is curious, has a quirky sense of humour, and loves chicken")
-reson = Cat.create(birthdate: "2019-09-23", color: "white", name: "Reson", sex: "F", description: "This cat is laid back, has a dark sense of humour, and loves to watch TV")
-gideon = Cat.create(birthdate: "2016-06-01", color: "brown", name: "Gideon", sex: "M", description: "This cat is easygoing, loves slapstick humour, and enjoys scrolling through Instagram")
+caesar = Cat.create(birth_date: "2015-09-08", color: "black", name: "Caesar", sex: "M", description: "This cat is curious, has a quirky sense of humour, and loves chicken")
+cleopatra = Cat.create(birth_date: "2019-09-23", color: "white", name: "Cleopatra", sex: "F", description: "This cat is laid back, has a dark sense of humour, and loves to watch TV")
+aurelius = Cat.create(birth_date: "2016-06-01", color: "brown", name: "Aurelius", sex: "M", description: "This cat is easygoing, loves slapstick humour, and enjoys scrolling through Instagram")
 
-CatRentalRequest.create(cat_id: ramsey.id, start_date: Date.today, end_date: Date.today + 3, status: "APPROVED")
-CatRentalRequest.create(cat_id: ramsey.id, start_date: Date.today + 4, end_date: Date.today + 7, status: "APPROVED")
-CatRentalRequest.create(cat_id: reson.id, start_date: Date.today, end_date: Date.today + 3)
-CatRentalRequest.create(cat_id: gideon.id, start_date: Date.today, end_date: Date.today + 3, status: "DENIED")
+CatRentalRequest.create(cat_id: caesar.id, start_date: Date.today, end_date: Date.today + 3, status: "APPROVED")
+CatRentalRequest.create(cat_id: caesar.id, start_date: Date.today + 4, end_date: Date.today + 7, status: "APPROVED")
+CatRentalRequest.create(cat_id: cleopatra.id, start_date: Date.today, end_date: Date.today + 3)
+CatRentalRequest.create(cat_id: aurelius.id, start_date: Date.today, end_date: Date.today + 3, status: "DENIED")
 
 
 
